980ef6c2b10e5156b16ed673e76f99eee0c24317
tsilva <tsilva@uplexis.com.br>
2020-05-05 19:54:38 -0300

    refactor: melhorando nomes de variaveis, semantica html e feat de mostrar data

--- javascript.js
+++ javascript.js
@@ -1,78 +1,72 @@
+function calculate(){
+  var response = document.querySelector('.response')
+  var speed = Number(document.querySelector('.speed').value)
 
+  response.innerHTML = `A sua velocidade é de ${speed} Km </br>`
 
+  if (speed > 60) {
+    response.innerHTML += 'Você foi multado </br></br> Preencha os campos abaixo'
+  }
+  if (speed < 60) {
+    response.innerHTML += 'Não preencha os campos abaixo'
+  }
+}
 
-function calcular(){
-    var txtv = document.querySelector('.txtv')
-    var res = document.querySelector('.res')
-    var vel = Number(txtv.value)
-    res.innerHTML = `A sua velocidade é de ${vel} Km </br>`
-    if (vel > 60) {
-       res.innerHTML += 'Você foi multado</br>'
-       res.innerHTML += 'Preencha os campos abaixo'
-    }else if (vel < 60){
-        res.innerHTML += 'Não preencha os campos abaixo'
+function loading(){
+  var message = document.querySelector('.message')
+  var dateField = document.querySelector('.dateField')
+  var date = new Date()
+  var day = date.getDay()
+  var month = date.getMonth()
+  var year = date.getUTCFullYear()
+  var hour = date.getHours()
+  var minutes = date.getMinutes()
+
+  dateField.innerHTML = `São Bernardo do Campo, ${day}/${month}/${year} ${hour}:${minutes.length === 1 ? '0' + minutes : minutes }`
+  message.innerHTML = 'Seja Bem Vindo'
+}
+
+function search(){
+  var date = new Date
+  var year = date.getFullYear()
+  var ageField = document.querySelector('.ageField')
+  var ida = document.querySelector('.ida')
+
+  if ( ageField.value.length == 0 || Number(ageField.value) > year ) {
+    window.alert('[erro] verificar os dados e tente novamente')  
+  } else {
+    var radioSex = document.getElementsByName('radioSex')
+    var age = year - Number(ageField.value)
+    var gender = ''  
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+
+    if (radioSex[0].checked) {
+      gender = 'homem'
+      if (age >= 0 && age < 10) {
+        img.setAttribute('src' , 'fotobbmasc1.jpg')
+      } else if(age < 21) {
+        img.setAttribute('src' , 'jovemhom1.jpg')
+      } else if(age < 50) {
+        img.setAttribute('src' , 'adultohom1.jpg')
+      } else {
+        img.setAttribute('src' , 'vovohom1.jpg')
+      }
+    } else if (radioSex[1].checked){
+      gender = 'mulher' 
+
+      if (age >= 0 && age < 10){
+        img.setAttribute('src' , 'fotobbfem1.jpg')
+      } else if (age < 21 ) {
+        img.setAttribute('src' , 'jovemfem2.jpg' )
+      } else if (age < 50) {
+        img.setAttribute('src' , 'adulto.fem2.jpg')
+      } else {
+        img.setAttribute('src' , 'vovofem2.jpg')
+      }
     }
-  
-}
 
-function carregamento(){
-    var msg = document.querySelector('.mensagem')
-    var dta = document.querySelector('.dta')
-    var data = new Date()
-    var dia = data.getDay()
-    var mes = data.getMonth()
-    var ano = data.getUTCFullYear()
-    var hora = data.getHours()
-    var min = data.getMinutes()
-    dta.innerHTML = `São Bernardo do Campo  Dia/${dia}/${mes}/${ano} ${hora}:${min}`
-    msg.innerHTML = 'Seja Bem Vindo'
-}
-
-function procurar(){
-   var data = new Date
-   var ano = data.getFullYear()
-   var fano = document.querySelector('.txt2')
-   var ida = document.querySelector('.ida')
-   if ( fano.value.length == 0 || Number(fano.value) > ano ) {
-     window.alert('[erro] verificar os dados e tente novamente')  
-   } else {
-       var fsex = document.getElementsByName('radsex')
-       var idade = ano - Number(fano.value)
-       var genero = ''  
-       var img = document.createElement('img')
-       img.setAttribute('id', 'foto')
-       if (fsex[0].checked) {
-            genero = 'homem'
-            if (idade >= 0 && idade < 10){
-                img.setAttribute('src' , 'fotobbmasc1.jpg')
-    
-            }else if(idade < 21){
-                img.setAttribute('src' , 'jovemhom1.jpg')
-            }
-             else if(idade < 50){
-                img.setAttribute('src' , 'adultohom1.jpg')
-            }else{
-                img.setAttribute('src' , 'vovohom1.jpg')
-            }
-        
-        } else if (fsex[1].checked){
-                genero = 'mulher' 
-                if (idade >= 0 && idade < 10){
-                    img.setAttribute('src' , 'fotobbfem1.jpg')
-                }else if (idade < 21 ) {
-                    img.setAttribute('src' , 'jovemfem2.jpg' )
-                }else if (idade < 50) {
-                    img.setAttribute('src' , 'adultofem2.jpg')
-                }else{
-                    img.setAttribute('src' , 'vovofem2.jpg')
-                }
-            
-
-        }
-        ida.innerHTML = `Detectamos ${genero} com ${idade} anos`
-        ida.appendChild(img)
-
-   }
-
-
+    ida.innerHTML = `Detectamos ${gender} com ${age} anos`
+    ida.appendChild(img)
+  }
 }
